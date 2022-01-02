8c5d2a25ab2db6d447f1f4c2e8dc12ef9c13ea40
Tessa Hayden <tessahayden@Tessas-MacBook-Air.local>
2022-01-02 10:29:57 -0800

    Workshop Week 2

---
+++ models/promotion.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+require("mongoose-currency").loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const promotionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+    },
+    cost: {
+      type: Currency,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = mongoose.model("Promotions", promotionSchema);
--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,45 +1,83 @@
 const express = require('express');
-const partners = express.Router();
+const Partner = require('../models/partner');
 
-partners
+const partnerRouter = express.Router();
+
+partnerRouter
   .route("/")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
+  .get((req, res, next) => {
+    Partner.find()
+      .then((partners) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(partners);
+      })
+      .catch((err) => next(err));
   })
-  .get((req, res) => {
-    res.end("Will send all the campsites to you");
-  })
-  .post((req, res) => {
-    res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
-    );
+  .post((req, res, next) => {
+    Partner.create(req.body)
+      .then((partner) => {
+        console.log("Partner Created ", partner);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(partner);
+      })
+      .catch((err) => next(err));
   })
   .put((req, res) => {
     res.statusCode = 403;
-    res.end("PUT operation not supported on /campsites");
+    res.end("PUT operation not supported on /partners");
   })
-  .delete((req, res) => {
-    res.end("Deleting all campsites");
+  .delete((req, res, next) => {
+    Partner.deleteMany()
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
   });
 
-partners
+partnerRouter
   .route("/:partnerId")
-  .get((req, res) => {
-    res.end("Will send all the campsites to you");
+  .get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+      .then((partner) => {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(partner);
+      })
+      .catch((err) => next(err));
   })
   .post((req, res) => {
+    res.statusCode = 403;
     res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+      `POST operation not supported on /partners/${req.params.partnerId}`
     );
   })
-  .put((req, res) => {
-    res.statusCode = 403;
-    res.end("PUT operation not supported on /campsites");
+  .put((req, res, next) => {
+    Partner.findByIdAndUpdate(
+      req.params.partnerId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then((partner) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(partner);
+      })
+      .catch((err) => next(err));
   })
-  .delete((req, res) => {
-    res.end("Deleting all campsites");
+  .delete((req, res, next) => {
+    Partner.findByIdAndDelete(req.params.partnerId)
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
   });
 
-module.exports = partners;
+module.exports = partnerRouter;
