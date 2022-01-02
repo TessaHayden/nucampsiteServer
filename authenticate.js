289d8a79a611f46a59e83ecd1043bf9160b5b44b
Tessa Hayden <tessahayden@Tessas-MacBook-Air.local>
2022-01-02 14:57:13 -0800

    Passport

---
+++ authenticate.js
@@ -0,0 +1,7 @@
+const passsport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
